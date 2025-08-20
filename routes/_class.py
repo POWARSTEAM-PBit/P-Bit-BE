df9e519b95bfc2b4b4f15f80a60ac5b3394f8a3b
Lucas Aponso <s3896348@student.rmit.edu.au>
2025-08-20 13:36:52 +1000

    mod: created type for create class

--- routes/_class.py
+++ routes/_class.py
@@ -6,11 +6,22 @@ from email_validator import validate_email, EmailNotValidError
 from db.init_engine import get_db, engine
 from db import db_models
 from utils import api_resp, error_resp
+from typing import List
 
 db_models.Base.metadata.create_all(bind=engine)
+
+class create_class_input(BaseModel):
+    class_name: str
+    class_description: str
+    tag: List[str]
 
 router = APIRouter(prefix="/class")
 
 @router.post("/create", tags=["class"], status_code=status.HTTP_201_CREATED, responses={})
-async def register(payload: str, db: Session = Depends(get_db)):
-    print("HELLO")
+async def create_class(payload: create_class_input, db: Session = Depends(get_db)):
+    ## check that user_id exists in user table
+
+    ## Check that the valid user has already created a class with the same name if so
+    ##then dont allow
+
+    
