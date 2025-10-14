9baea566dfb8241c4131331d8ceb9151a8857f66
Lucas Aponso <s3896348@student.rmit.edu.au>
2025-10-14 17:39:48 +1100

    mod: updated nginx for btter config

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ frontend_origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=frontend_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"],
     allow_headers=[
